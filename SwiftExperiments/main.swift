b5c7009c29695ca9f6d0f92ea10d11dd278a1008
David Karnok <akarnokd@Davids-iMac.local>
2017-01-05 15:10:02 +0100

    Initial Commit

---
+++ SwiftExperiments/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftExperiments
+//
+//  Created by David Karnok on 2017. 01. 05..
+//  Copyright © 2017. David Karnok. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
